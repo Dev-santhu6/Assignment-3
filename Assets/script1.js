6490958930a861a4ce1e4bbbc6febe2e22c8a2b0
Balamuralikirushnan Santhujan <muralisanthu696@gmail.com>
2023-11-14 06:41:50 +0530

    add my project2.0

--- Assets/script1.js
+++ Assets/script1.js
@@ -17,31 +17,28 @@
 // darkmoodset
 let toggle = document.getElementById("toggleDark");
 let body = document.querySelector("body");
-let nav1 = document.getElementById("navbar1");
 
 toggle.addEventListener('click',function(){
     this.classList.toggle("bi-moon");
     if(this.classList.toggle("bi-brightness-high")){
         body.style.background = "white";
         body.style.color = "black";
-        nav1.style.color = "black";
         body.style.transition = "2s";
     }else{
         body.style.background ="black";
         body.style.color = "white";
-        nav1.style.color = "white";
         body.style.transition = "2s";
     }
 
 })
 
 // map
-function initMap() {
-  var options= {
-    zoom:20,
+function iniMap() {
+  var opt= {
+    zoom:15,
     center:{lat:9.657770,lng:80.159210}
   }
-  var googlemap = new google.maps.Map(document.getElementById("map"), options);
+  var googlemap = new google.maps.Map(document.getElementById("map"), opt);
 }
 
 //faq
